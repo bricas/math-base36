e238f4577c293e630f44e2dd9ef05e2a129311a3
Brian Cassidy <bricas@cpan.org>
2013-03-14 21:06:56 -0300

    prep release

--- lib/Math/Base36.pm
+++ lib/Math/Base36.pm
@@ -10,7 +10,7 @@ use Math::BigInt ();
 our %EXPORT_TAGS = ( 'all' => [ qw(encode_base36 decode_base36) ] );
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{ 'all' } } );
 
-our $VERSION = '0.10';
+our $VERSION = '0.11';
 
 sub decode_base36 {
     my $base36 = uc( shift );
@@ -87,7 +87,7 @@ Brian Cassidy E<lt>bricas@cpan.orgE<gt>
 
 Copyright 2002 by Rune Henssel
 
-Copyright 2007-2012 by Brian Cassidy
+Copyright 2007-2013 by Brian Cassidy
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself. 
